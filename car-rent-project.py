c541013e547173b80ede7f3996ef53960cb35992
Yadieldiazthen <yodidiazhen@gmail.com>
2026-04-29 20:36:02 -0400

    finishing

--- car-rent-project.py
+++ car-rent-project.py
@@ -3,11 +3,7 @@ con = sqlite3.connect("Cars.db")
 
 cur = con.cursor()
 
-cars = []
-customers = []
-rents = []
-
-def cars_database():
+def cars_view():
     cur.execute("SELECT * FROM Cars")
 
     filas = cur.fetchall()
@@ -16,91 +12,87 @@ def cars_database():
         print(fila)
 
 def new_cars():
-    name = input("Car name:")
+    Year = input("Car year:")
+    Model = input("Car model:")
+    Brand = input("Car brand:")
 
-    car = {"name":name, "state": "Available"}
+    cur.execute("""
+    INSERT INTO cars (brand, model, year, state)
+    VALUES (?, ?, ?, 'Available')
+    """, (Brand, Model, Year))
 
+    con.commit()
     
     print("Car added")
 
-def view_cars():
-    if len(cars) == 0:
-        print("No cars")
-    else:
-        for car in cars:
-            print(car["name"], "is", car["state"])
-
 def new_customers():
-    name = input("Costumer name:")
+    Name = input("Customer name:")
 
-    if name == "":
-        print("write a properly name")
-        return
-    customer = {'name': name}
-    customers.append(customer)
-    print("Costumer added")
+    cur.execute("INSERT INTO Customers (Name) VALUES (?)",
+    (Name,))
+
+    con.commit()
+    
+    print("Customer added")
 
 def view_customers():
-    if len(customers) == 0:
-        print("No customers")
-    else:
-        for i, c in enumerate(customers):
-            print(i,"-",c["name"])
+    cur.execute("SELECT * FROM Customers")
+
+    filas = cur.fetchall()
+
+    for fila in filas:
+        print(fila)
+
         
 def rent_car():
-    if len(cars) == 0 or len(customers) == 0:
-        print("Add a car and a customer")
+    cur.execute("SELECT * FROM Cars WHERE state = 'Available' ")
+    cars = cur.fetchall()
+    
+    if len(cars) == 0:
+        print("No cars")
         return
     
     print("Cars:")
-    for i, car in enumerate(cars):
-        print(i, car["name"], car['state'])
+    for car in cars:
+        print(car)
+        
     try: 
-        car_number = int(input("Choose car number:"))
+        car_id = int(input("Choose car id:"))
     except: 
         print("Type a properly number")
         return   
-    if car_number < 0 or car_number >= len(cars):
-        print("Write a properly number")
+    
+    cur.execute("SELECT * FROM Customers")
+    customers = cur.fetchall()
+
+    if len(customers) == 0:
+        print("No customers")
         return  
-    if cars[car_number]['state'] != 'Available':
-        print("Car is already rented")
-        return 
+    
     print("Customers:")
-    for i, c in enumerate(customers):
-        print(i, c['name'])
+    for custo in customers:
+        print(custo)
+    
     try:
-        customer_number = int(input("Choose customer number:"))
+        customer_id = int(input("Choose customer id:"))
     except:
         print("Choose a properly number:")
         return
-    if customer_number < 0 or customer_number >= len(customers):
-        print("Incorrect")
-        return
-    cars[car_number]['state'] = 'Rented'
+    
+    cur.execute("""INSERT INTO Rents (car_id, customer_id) VALUES (?, ?)""", (car_id, customer_id))
+    cur.execute("""UPDATE Cars SET state = 'Rented' WHERE id = ? """, (car_id,))
 
-    rental = {'car': cars[car_number]['name'], 'customer': customers[customer_number]['name']}
+    con.commit()
 
-    rents.append(rental)
     print("Car rented")
 
-def view_rents():
-    if len(rents) == 0:
-        print("No rents")
-    else:
-        for r in rents:
-            print(r["customer"], "rented", r["car"])
-
 def return_car():
-    print("Cars:")
-    for i, car in enumerate(cars):
-        print(i, car['name'], car['state'])
+    car_id = int(input("Enter car Id to return:"))
 
-    car_number = int(input('Choose you car:'))
+    cur.execute("""UPDATE Cars SET state = 'Available' Where id = ? """, (car_id,))
+    con.commit()
 
-    cars[car_number]['state'] = 'Available'
     print("Car was perfectly returned")
-
 
 while True:
     print("RENT CAR")
@@ -109,17 +101,15 @@ while True:
     print("3. Add Customer:")
     print("4. View customers:")
     print("5. Rent car:")
-    print("6. View rents:")
-    print("7. Return Car:")
-    print("8. View Database cars")
-    print("9. Exit:")
+    print("6. Return Car:")
+    print("7. Exit:")
 
     select = input("Choose a number:")
 
     if select == "1":
         new_cars()
     elif select == "2":
-        view_cars()
+        cars_view()
     elif select == "3":
         new_customers()
     elif select == "4":
@@ -127,12 +117,8 @@ while True:
     elif select == '5':
         rent_car()
     elif select == '6':
-        view_rents()
-    elif select == '7':
         return_car()
-    elif select == '8':
-        cars_database()
-    elif select == '9':
+    elif select == '7':
         break
     else:
         print("Try again")
